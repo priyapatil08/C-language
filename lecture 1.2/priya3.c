fa7a3054fddf940dfdddb21bd0e6633661dfc0e6
Priya Patil <patilpriyas445@gmail.com>
2024-07-18 21:59:17 +0530

    Add files via upload

---
+++ lecture 1.2/priya3.c
@@ -0,0 +1,12 @@
+//write a program to print the given pattern.
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	printf("*\n");
+	printf("**\n");
+	printf("***\n");
+	printf("**\n");
+	printf("*\n");
+}
