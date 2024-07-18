db683b8f2ad957c825708595937d482f0ccc0563
Priya Patil <patilpriyas445@gmail.com>
2024-07-18 21:55:14 +0530

    Add files via upload

---
+++ lecture 1.2/priya1.c
@@ -0,0 +1,10 @@
+//write a program to print your age , name and school.
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	printf("Name : Patil priya \n");
+	printf("age: 18\n");
+	printf("school:red and white\n");
+}
---
+++ lecture 1.2/priya4.c
@@ -0,0 +1,18 @@
+//write a program to print the given pattern.
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	printf("*\n");
+	printf("*\n");
+	printf("*\n");
+	printf("*           * *           *\n");
+	printf("*         *     *        * \n");
+	printf("*       *         *     *  \n");
+	printf("*     *             *  *   \n");
+	printf("*   *                      \n");
+	printf("* *                        \n");
+	printf("*                          \n");
+	
+}
