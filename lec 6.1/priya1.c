3ba52b5f577092ced034b995769442808e9b7b45
Priya Patil <patilpriyas445@gmail.com>
2024-08-01 21:35:14 +0530

    Add files via upload

---
+++ lec 6.1/priya1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int num1;
+	printf("Enter A :");
+	scanf("%d",&num1);
+	
+	int num2;
+	printf("Enter B :");
+	scanf("%d",&num2);
+	
+	(num1<num2)? printf("num1 is minimum!"):printf("num2 is minimum");
+}
