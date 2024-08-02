1ef03c1b1381329811cde78aa1867c501aa49d33
Priya Patil <patilpriyas445@gmail.com>
2024-08-02 18:38:00 +0530

    Add files via upload

---
+++ max&min/priya3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a,b,c,d;
+	
+	printf("Enter A :");
+	scanf("%d",&a);
+	printf("Enter B :");
+	scanf("%d",&b);
+	printf("Enter C :");
+	scanf("%d",&c);
+	printf("Enter D :");
+	scanf("%d",&d);
+	
+	(a>b)?(a>c)?(a>d)?printf("a is maximum!"):printf("c is maximum!"):printf("d is maximum!"):
+	(b>c)?(b>d)?printf("b is maximum!"):printf("c is maximum!"):
+	(c>d)?printf("c is maximum!"):printf("d is maximum!");	
+	
+}
