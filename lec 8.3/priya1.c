aa7f4c11c7de5cd51f859bd7c2db61ebde138208
Priya Patil <patilpriyas445@gmail.com>
2024-09-06 12:16:52 +0530

    Add files via upload

---
+++ lec 8.3/priya1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int i;
+	printf("Enter n :");
+	scanf("%d",&i);
+	
+	do
+	{
+	    printf("%d ",i);	
+		i++;
+	}while(i<=10);
+	
+}
---
+++ lec 8.3/priya2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int i;
+	int n;
+	
+	printf("enter the value of n=");
+	scanf("%d",&n);
+	
+	
+	for(i=n; i>=1; i--)
+	{
+	    printf("%d ",i);
+			
+	}
+}
