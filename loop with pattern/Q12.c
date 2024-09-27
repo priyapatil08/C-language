42b2d758a34b31af703ffceae1b527d95d09d315
Priya Patil <patilpriyas445@gmail.com>
2024-09-27 15:42:33 +0530

    Add files via upload

---
+++ loop with pattern/Q12.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int i,j;
+	char ;
+	for(i='A'; i<='E'; i++)
+	{
+		for(j='A'; j<=i; j++)
+		{
+			printf("%c ",j);
+		}
+		printf("\n");
+	}
+}
---
+++ loop with pattern/Q13.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int i,j,k='A';
+	char ;
+	for(i='A'; i<='E'; i++)
+	{
+		for(j='A'; j<=i; j++)
+		{
+			printf("%c ",k);
+			k++;
+		}
+		printf("\n");
+	}
+}
