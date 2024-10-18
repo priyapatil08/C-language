53e821b7ed26eb0c77e9d47ba3676928f74bb3f2
Priya Patil <patilpriyas445@gmail.com>
2024-10-18 18:32:06 +0530

    Add files via upload

---
+++ lec 2.2/Q1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	printf("Name:Patil Priya\n");
+	printf("Age:18\n");
+	printf("School:Red and White\n");
+}
---
+++ lec 2.2/Q2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	printf(" ------\n");
+	printf("|      |\n");
+	printf("|      |\n");
+	printf("| NAME |\n");
+	printf("|      |\n");
+	printf("|      |\n");
+	printf(" ------\n");
+}
