dc637c179ab4db95774c5a68b1330f62882056b0
Priya Patil <patilpriyas445@gmail.com>
2024-11-18 18:57:54 +0530

    Add files via upload

---
+++ 2d array/q3.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int r,c,i,j,sum=0;
+	
+	printf("Enter r :");
+	scanf("%d",&r);
+	
+	printf("Enter c :");
+	scanf("%d",&c);
+	
+	int a[r][c];
+	
+	for(i=0; i<r; i++)
+	{
+		for(j=0; j<c; j++)
+		{
+			printf("Enter a[%d][%d]",i,j);
+			scanf("%d",&a[i][j]);
+		}
+	}
+	
+	printf("\n");
+	
+	
+	
+	printf("\n");
+	
+	for(i=0; i<r; i++)
+	{
+		for(j=0; j<c; j++)
+		{
+			printf("%d",a[i][j]);
+		}
+		printf("\n");
+	}
+
+	
+	for(i=0; i<r; i++)
+	{
+		for(j=0; j<c; j++)
+		{
+			if(i==j)
+			{
+				sum = sum + a[i][j];
+			}
+		
+		}
+	}
+	
+	
+	printf("diagonal is %d",sum);
+	
+	
+	
+	
+
+}
