dba138a2f6b96d466fa960876f95c571d64cec65
Priya Patil <patilpriyas445@gmail.com>
2024-11-26 18:08:08 +0530

    Add files via upload

---
+++ Array/Q2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int n;
+	printf("Enter n:");
+	scanf("%d",&n);
+	
+	int a[n];
+	
+	int i;
+	for(i=0; i<n; i++)
+	{
+		printf("Enter a[%d]:",i);
+		scanf("%d",&a[i]);
+	}
+	
+	for(i=0; i<n; i++)
+	{
+		if(a[i]%2==0)
+		{
+			printf("%d ",a[i]);
+		}
+		
+		
+	}
+	
+}
+
